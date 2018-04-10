beab101f8dfcb99b1fbcfac501166ebdbdee324d
Wonakee Amos <wonakeeamos@gmail.com>
2018-04-10 20:14:50 +0000

    Final Commit

--- app/views.py
+++ app/views.py
@@ -64,6 +64,13 @@ def login():
 @login_required
 def secure_page():
     return render_template('secure_page.html')
+    
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.','danger')
+    return redirect(url_for('home'))
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
